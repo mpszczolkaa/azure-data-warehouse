9552d262bae11bd70e1be69a363f35e3f6d49763
Marcin Pszczółka <mikkser7676@gmail.com>
2026-09-06 23:56:08 +0200

    Create SP_DimProduct.sql

---
+++ sql/stored-procedures/SP_DimProduct.sql
@@ -0,0 +1,90 @@
+CREATE PROCEDURE SP_DimProduct
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+  
+
+    INSERT INTO dw.DimProduct
+    (
+        ProductID,
+        ProductName,
+        ProductNumber,
+        Color,
+        Size,
+        ListPrice,
+        SubcategoryID,
+        SubcategoryName,
+        CategoryID,
+        CategoryName
+    )
+    SELECT
+        p.ProductID,
+        p.Name,
+        p.ProductNumber,
+        p.Color,
+        p.Size,
+        p.ListPrice,
+        s.ProductSubcategoryID,
+        s.Name,
+        c.ProductCategoryID,
+        c.Name
+    FROM dbo.stg_ProductionProduct_CDC p
+
+    LEFT JOIN dbo.stg_ProductSubcategory s
+        ON p.ProductSubcategoryID = s.ProductSubcategoryID
+
+    LEFT JOIN dbo.stg_ProductCategory c
+        ON s.ProductCategoryID = c.ProductCategoryID
+
+    WHERE p.__$operation = 2
+
+      AND NOT EXISTS
+      (
+          SELECT 1
+          FROM dw.DimProduct d
+          WHERE d.ProductID = p.ProductID
+      );
+
+
+
+
+    UPDATE d
+    SET
+        d.ProductName = p.Name,
+        d.ProductNumber = p.ProductNumber,
+        d.Color = p.Color,
+        d.Size = p.Size,
+        d.ListPrice = p.ListPrice,
+
+        d.SubcategoryID = s.ProductSubcategoryID,
+        d.SubcategoryName = s.Name,
+
+        d.CategoryID = c.ProductCategoryID,
+        d.CategoryName = c.Name
+
+    FROM dw.DimProduct d
+
+    INNER JOIN dbo.stg_ProductionProduct_CDC p
+        ON d.ProductID = p.ProductID
+
+    LEFT JOIN dbo.stg_ProductSubcategory s
+        ON p.ProductSubcategoryID = s.ProductSubcategoryID
+
+    LEFT JOIN dbo.stg_ProductCategory c
+        ON s.ProductCategoryID = c.ProductCategoryID
+
+    WHERE p.__$operation = 4;
+
+
+
+
+    DELETE d
+    FROM dw.DimProduct d
+
+    INNER JOIN dbo.stg_ProductionProduct_CDC p
+        ON d.ProductID = p.ProductID
+
+    WHERE p.__$operation = 1;
+
+END;

5d2439f23cc35dd383e32cd0505affbf8d67ad32
Marcin Pszczółka <mikkser7676@gmail.com>
2026-09-07 13:11:29 +0200

    Create enable_cdc.sql

---
+++ sql/cdc/enable_cdc.sql
@@ -0,0 +1,33 @@
+
+--ENABLE FOR DATABASE--
+EXEC sys.sp_cdc_enable_db;
+
+--CDC table Sales.Customer--
+EXEC sys.sp_cdc_enable_table
+    @source_schema = 'Sales',
+    @source_name = 'Customer',
+    @role_name = NULL;
+
+--CDC table Person.Person--
+EXEC sys.sp_cdc_enable_table
+    @source_schema = 'Person',
+    @source_name = 'Person',
+    @role_name = NULL;
+
+--CDC table Production.Product
+EXEC sys.sp_cdc_enable_table
+    @source_schema = 'Production',
+    @source_name = 'Product',
+    @role_name = NULL;
+
+--CDC table Sales.SalesOrderHeader--
+EXEC sys.sp_cdc_enable_table
+    @source_schema = 'Sales',
+    @source_name = 'SalesOrderHeader',
+    @role_name = NULL;
+
+--CDC table Sales.OrderDetail
+EXEC sys.sp_cdc_enable_table
+    @source_schema = 'Sales',
+    @source_name = 'SalesOrderDetail',
+    @role_name = NULL;
